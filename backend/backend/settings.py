1cd6716c1193ea82f1afe16cf1558dbd4d69aa50
callmepri <140776053+callmepri@users.noreply.github.com>
2025-06-04 19:04:39 +1000

    feat: added new app to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'poker'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
--- backend/poker/apps.py
+++ backend/poker/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class PokerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'poker'
+    name = 'poker' 
+    verbose_name = 'Poker Game'
